fa710e0730de99d0e2122621ab920b9582d754ca
Mujahid Ansari <mujahid.ansari@prospecta.com>
2019-06-24 00:00:45 +0530

    header component commite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  myname='i m md mujahid ansari'
+  onclick1(){
+    alert("clik me reght");
+  }
 }
